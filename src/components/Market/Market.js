877afd9e374851d687bc36d4e5006306f3103f9c
chip <chiposaur@gmail.com>
2020-12-06 16:54:00 -0600

    buy sell buttons

--- src/components/Market/Market.js
+++ src/components/Market/Market.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
+import styled from "styled-components";
 
 import "./Market.scss"
 
@@ -21,57 +21,57 @@ const Market = () => {
 						<td>991</td>
 						<td>9999999</td>
 						<td>Fluxnet</td>
-						<td>lovendale0@w3.org</td>
-						<td>7850 Old Shore Drive</td>
+						<td className="buysell-cell"><button className="buysell-button">buy</button></td>
+						<td className="buysell-cell"><button className="buysell-button">sell</button></td>
 					</tr>
 					<tr>
 						<td>992</td>
 						<td>9999999</td>
 						<td>Solo</td>
-						<td>itassell1@ow.ly</td>
-						<td>245 Merchant Circle</td>
+						<td className="buysell-cell"><button className="buysell-button">buy</button></td>
+						<td className="buysell-cell"><button className="buysell-button">sell</button></td>
 					</tr>
 					<tr>
 						<td>993</td>
 						<td>9999999</td>
 						<td>Zillion</td>
-						<td>emercer2@ow.ly</td>
-						<td>70700 Kipling Pass</td>
-					</tr>
-					<tr>
-						<td>994</td>
-						<td>9999999</td>
-						<td>Altered Calm</td>
-						<td>cwhitley3@wsj.com</td>
-						<td>03 Service Terrace</td>
-					</tr>
-					<tr>
-						<td>995</td>
-						<td>9999999</td>
-						<td>Nails</td>
-						<td>csmitheram4@rambler.ru</td>
-						<td>9 Eliot Parkway</td>
-					</tr>
-					<tr>
-						<td>996</td>
-						<td>9999999</td>
-						<td>Hyfit</td>
-						<td>bsked5@51.la</td>
-						<td>03418 Ludington Plaza</td>
+						<td className="buysell-cell"><button className="buysell-button">buy</button></td>
+						<td className="buysell-cell"><button className="buysell-button">sell</button></td>
 					</tr>
 					<tr>
 						<td>997</td>
 						<td>9999999</td>
 						<td>Valeradone</td>
-						<td>hcrother6@opera.com</td>
-						<td>7932 Sloan Park</td>
+						<td className="buysell-cell"><button className="buysell-button">buy</button></td>
+						<td className="buysell-cell"><button className="buysell-button">sell</button></td>
+					</tr>
+					<tr>
+						<td>994</td>
+						<td>9999999</td>
+						<td>Altered Calm</td>
+						<td className="buysell-cell"><button className="buysell-button">buy</button></td>
+						<td className="buysell-cell"><button className="buysell-button">sell</button></td>
+					</tr>
+					<tr>
+						<td>995</td>
+						<td>9999999</td>
+						<td>Nails</td>
+						<td className="buysell-cell"><button className="buysell-button">buy</button></td>
+						<td className="buysell-cell"><button className="buysell-button">sell</button></td>
+					</tr>
+					<tr>
+						<td>996</td>
+						<td>9999999</td>
+						<td>Hyfit</td>
+						<td className="buysell-cell"><button className="buysell-button">buy</button></td>
+						<td className="buysell-cell"><button className="buysell-button">sell</button></td>
 					</tr>
 					<tr>
 						<td>998</td>
 						<td>9999999</td>
 						<td>Nanozine</td>
-						<td>tajean7@sfgate.com</td>
-						<td>2 Schurz Junction</td>
+						<td className="buysell-cell"><button className="buysell-button">buy</button></td>
+						<td className="buysell-cell"><button className="buysell-button">sell</button></td>
 					</tr>
 				</tbody>
 			</table>
