15e93ce31476405ac0061b8d7178053d4775b309
Darrin Lowery <ogrotten@monsto.com>
2021-05-08 09:33:44 -0500

    dialogs

--- src/components/Money/index.js
+++ src/components/Money/index.js
@@ -1,5 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { GameContext } from "../../contexts/GameContext"
+
+import useModal from "../../hooks/useModal"
+import Modal from "../Modal"
 
 // import styled from "styled-components";
 
@@ -8,13 +11,59 @@ import { GameContext } from "../../contexts/GameContext"
 import "./Money.scss"
 
 const Money = (props) => {
-	const { playerState: { cash, bank, debt } } = useContext(GameContext)
+	const [data, setData] = useState({})
+	const { playerState: { cash, bank, debt, position } } = useContext(GameContext)
+	const { modalShow, modalHide, isShowing } = useModal()
+
+	const handleBank = () => {
+		setData({
+			title: "Gonk National Bank",
+			type: "Bank",
+			eventAction,
+		})
+		modalShow()
+	}
+	const handleShark = () => {
+		setData({
+			title: "Fast Eddies, LLC",
+			type: "Shark",
+			eventAction,
+		})
+		modalShow()
+	}
+
+	const eventAction = () => {
+
+	}
 
 	return (
 		<section className="money">
 			<div className="cash">Cash: {cash}</div>
-			<div className="bank">Bank: {bank}</div>
-			<div className="debt">Debt: {debt}</div>
+			<div className="bank">
+				Bank: {bank}
+				{position === 1 &&
+					<>
+						<br />
+						<button className="buysell-button" onClick={handleBank}>Go to Bank</button>
+					</>
+				}
+			</div>
+			<div className="debt">
+				Debt: {debt}
+				{position === 1 &&
+					<>
+						<br />
+						<button className="buysell-button" onClick={handleShark}>Visit Loan Shark</button>
+					</>
+				}
+			</div>
+			<Modal data={data}
+				isShowing={isShowing}
+				hide={modalHide}
+				normal={false}
+			>
+				{data.type} Window
+			</Modal>
 		</section>
 	)
 }
