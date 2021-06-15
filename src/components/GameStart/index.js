47c78ca012d7dbefc200c5a5033125dd0f6b2604
Darrin Lowery <ogrotten@monsto.com>
2021-06-15 15:42:32 -0400

    .

--- src/components/GameStart/index.js
+++ src/components/GameStart/index.js
@@ -12,8 +12,11 @@ const GameStart = () => {
 
 	return (
 		<div>
+			<p>You're in a cyberpunk metropolis slingin snow, trying to pile up as much money as you can in a year of dealing, one week at a time.</p>
+			<p>Buy low, sell high. Drop your duckets into the bank for risk-free interest. Take on debt if you dare.</p>
+			<p>Maybe you can't take it with you, but you can sure as hell run up the score while you're in it.</p>
 			<button onClick={begin}>
-				git money
+				Git money!
 			</button>
 		</div>
 	)
