5d019492b8e2529878a86c82c5ff4f88ba45eecb
Darrin Lowery <ogrotten@monsto.com>
2021-06-29 11:11:01 -0400

    gamestart

--- src/components/GameStart/index.js
+++ src/components/GameStart/index.js
@@ -6,10 +6,17 @@ import { GameContext } from "../../contexts/GameContext"
 import "./GameStart.scss"
 
 const styles = {
-	Pregame: panache.div({
+	Pregame: panache.div(({ theme, media }) => ({
 		padding: "18%",
 		lineHeight: "200%",
-	})
+		p: {
+			marginTop: "0",
+		},
+		[media.small]: {
+			fontSize: "16px",
+			lineHeight: "175%",
+		},
+	}))
 }
 
 const GameStart = () => {
@@ -22,10 +29,10 @@ const GameStart = () => {
 	}
 
 	return (
-		<Pregame>
-			<p>You're coming up in a corporate metropolis, slingin snow and trying to pile up as many creds as you can in a year of dealing.</p>
-			<p>Buy low, sell high, one week at a time. Drop your duckets into the bank for risk-free interest. Or take on debt from the loan sharks for the one big score, if you dare.</p>
-			<p>Maybe you can't take it with you, but you can sure as hell run up the score and leave this city far behind.</p>
+		<Pregame id="pregame" >
+			<p id="p1">You're coming up in a corporate metropolis, slingin snow and trying to pile up as many creds as you can in a year of dealing.</p>
+			<p id="p2">Buy low, sell high, one week at a time. Drop your duckets into the bank for risk-free interest. Or take on debt from the loan sharks for the one big score, if you dare.</p>
+			<p id="p3">Maybe you can't take it with you, but you can sure as hell run up the score and leave this city far behind.</p>
 			<button onClick={begin}>
 				git money
 			</button>
